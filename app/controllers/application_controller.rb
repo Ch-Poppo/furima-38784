49483c45d3d6418113c5991b37ef8b2ea6b84471
Ch-Poppo <mtb.cannondale@icloud.com>
2023-04-17 21:55:56 +0900

    新規登録、ログイン、ログアウト機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :famiry_name, :first_name, :famiry_name_yomi, :first_name_yomi, :date_of_birth])
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         has_many :items
+         has_many :purchases
+
+         validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+         validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+         validates :nickname, presence: true
+         validates :first_name, presence: true
+         validates :famiry_name, presence: true
+         validates :first_name_yomi, presence: true
+         validates :famiry_name_yomi, presence: true
+         validates :date_of_birth, presence: true
+end
