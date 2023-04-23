ccf73f612f0a156a72963cb8e2c37fb47e8f711d
Ch-Poppo <mtb.cannondale@icloud.com>
2023-04-23 18:22:05 +0900

    商品詳細表示させた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
 
 
   def new
@@ -17,6 +17,10 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all.order("created_at DESC")
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
 
