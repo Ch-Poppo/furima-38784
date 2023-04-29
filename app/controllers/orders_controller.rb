302f42461a965c7690ed03a6a273b80ff9e7be82
Ch-Poppo <mtb.cannondale@icloud.com>
2023-04-29 23:37:48 +0900

    soldoutを表示させた

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, except: [:index]
   def index
     @purchase_address = PurchaseAddress.new
     @item = Item.find(params[:item_id])
--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -3,7 +3,4 @@ class Purchase < ApplicationRecord
   belongs_to :item
   has_one :address
 
-  # validates :user_id, presence: true消去予定
-  # validates :item_id, presence: true消去予定
-
 end
