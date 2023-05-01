3a9fef37bbba4c5a6a499b89f3d76f3952ccf462
Ch-Poppo <mtb.cannondale@icloud.com>
2023-05-01 16:25:53 +0900

    コードをまとめた

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,11 +4,8 @@ class OrdersController < ApplicationController
 
   def index
     @purchase_address = PurchaseAddress.new
-    if current_user == @item.user
-      redirect_to root_path
-      if @item.purchase
-        redirect_to root_path
-      end
+    if current_user == @item.user || @item.purchase.present?
+     redirect_to root_path
     end
   end
 
